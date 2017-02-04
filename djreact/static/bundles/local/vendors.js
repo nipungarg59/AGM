8a585d20cdb64eccda0f4b34b00d759467a3d7a6
nipungarg59 <nipungarg59@gmail.com>
2017-02-04 22:58:54 +0530

    minor changes

--- djreact/static/bundles/local/vendors.js
+++ djreact/static/bundles/local/vendors.js
@@ -76,7 +76,7 @@
 /******/ 			script.charset = 'utf-8';
 /******/ 			script.async = true;
 
-/******/ 			script.src = __webpack_require__.p + "" + chunkId + "." + ({"0":"App1","1":"MainApp"}[chunkId]||chunkId) + "-" + "10c6cdb6e9772b4f92de" + ".js";
+/******/ 			script.src = __webpack_require__.p + "" + chunkId + "." + ({"0":"App1","1":"MainApp"}[chunkId]||chunkId) + "-" + "3b061a149ba5506d2fa1" + ".js";
 /******/ 			head.appendChild(script);
 /******/ 		}
 /******/ 	};

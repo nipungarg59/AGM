0907fe51b8db8fe1b2258cb5ce56589ac6878804
nipungarg59 <nipungarg59@gmail.com>
2017-02-04 12:12:17 +0530

    Demo :p

--- djreact/static/bundles/local/vendors.js
+++ djreact/static/bundles/local/vendors.js
@@ -76,7 +76,7 @@
 /******/ 			script.charset = 'utf-8';
 /******/ 			script.async = true;
 
-/******/ 			script.src = __webpack_require__.p + "" + chunkId + "." + ({"0":"App1","1":"MainApp"}[chunkId]||chunkId) + "-" + "72fedf7bf090fc866afa" + ".js";
+/******/ 			script.src = __webpack_require__.p + "" + chunkId + "." + ({"0":"App1","1":"MainApp"}[chunkId]||chunkId) + "-" + "c1291d5b64c4aecddfd8" + ".js";
 /******/ 			head.appendChild(script);
 /******/ 		}
 /******/ 	};

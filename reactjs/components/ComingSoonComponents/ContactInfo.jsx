87eb4198b81a5a6104af65e36fa6de7f051619d8
nipungarg59 <nipungarg59@gmail.com>
2017-02-04 22:56:42 +0530

    minor changes

--- reactjs/components/ComingSoonComponents/ContactInfo.jsx
+++ reactjs/components/ComingSoonComponents/ContactInfo.jsx
@@ -6,11 +6,11 @@ export default class ContactInfo extends React.Component{
 			<div className="row">
 			    <div className="col-sm-4 animated bounceInLeft sblock contact">
 					    <i className="fa fa-phone fa-4x" ></i>
-					    <p style={{fontFamily: 'Raleway',font:'sans-serif',fontSize:"18px"}}>011- 65 25 4066 <br/> +91 - 99 99 16 4066</p>
+					    <p style={{fontFamily: 'Raleway',font:'sans-serif',fontSize:"18px"}}>011- 65 25 4066 <br/> +91 - 99 99 16 4066<br/>+91- 97 11 841285</p>
 			    </div>
 			     <div className="col-sm-4 animated bounceInUp sblock contact">
 					    <i className="fa fa-envelope-o fa-4x" ></i>
-					    <p style={{fontFamily: 'Raleway',font:'sans-serif',fontSize:"18px"}}>ca.akshaymittal @gmail.com</p>
+					    <p style={{fontFamily: 'Raleway',font:'sans-serif',fontSize:"18px"}}>ca_agm@hotmail.com</p>
 			    </div>
 			     <div className="col-sm-4 animated bounceInRight sblock contact">
 					    <i className="fa fa-map-marker fa-4x" aria-hidden="true"></i>

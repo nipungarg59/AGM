d21cf208fa083777a1ae5148bb0d862e2cab19f5
nipungarg59 <nipungarg59@gmail.com>
2017-04-10 18:11:49 +0530

    Added Compression

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,9 @@
-// var path = require("path")
-// var webpack = require('webpack')
-// var BundleTracker = require('webpack-bundle-tracker')
-
-// module.exports = {
-//   context: __dirname,
-
-//   entry: './assets/js/index', // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
-
-//   output: {
-//       path: path.resolve('./assets/bundles/'),
-//       filename: "[name]-[hash].js",
-//   },
-
-//   plugins: [
-//     new BundleTracker({filename: './webpack-stats.json'}),
-//   ],
-
-//   module: {
-//     loaders: [
-//       { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader',query: { presets:['react'] }}, // to transform JSX into JS
-//     ],
-//   },
-
-//   resolve: {
-//     modulesDirectories: ['node_modules', 'bower_components'],
-//     extensions: ['', '.js', '.jsx']
-//   },
-// }
-
 var path = require("path")
 var webpack = require('webpack')
 var BundleTracker = require('webpack-bundle-tracker')
 var config = require('./webpack.base.config.js')
 
-config.devtool = "#eval-source-map"
+// config.devtool = "#eval-source-map"
 
 config.plugins = config.plugins.concat([
   new BundleTracker({filename: './webpack-stats-local.json'}),
@@ -61,5 +31,24 @@ config.module.loaders.push(
   test: /\.(jpg|png)$/,
   loader: 'url?limit=10000&publicPath=./static/bundles/local/&name=[name].[ext]'
 })
+
+config.plugins = config.plugins.concat([
+
+  // removes a lot of debugging code in React
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+  }}),
+
+  // keeps hashes consistent between compilations
+  new webpack.optimize.OccurenceOrderPlugin(),
+
+  // minifies your code
+  new webpack.optimize.UglifyJsPlugin({
+    compressor: {
+      warnings: false
+    }
+  })
+])
 
 module.exports = config
